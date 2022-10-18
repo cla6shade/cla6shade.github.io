f33e6e46ef83596dceaf4370402dc3d643e0de77
cla6shade <cla6shade@gmail.com>
2022-10-19 02:38:05 +0900

    scene2까지 추가 완료

--- js/scene1.js
+++ js/scene1.js
@@ -1,24 +1,28 @@
-window.onload = function(){
-
-}
-function bind_scene1_button(){
+window.addEventListener('load', () => {
+    bind_scene1_buttons();
+});
+function bind_scene1_buttons(){
     let btn = document.getElementById("title-button");
     btn.onclick = function () {
         let s1_animate = [
-            {transform: "translate(0, 0)"},
-            {transform: "translate(0, -100vw)"}
+            {transform: "translate(0, 0)",
+            opacity: "1"},
+            {transform: "translate(100px, 0)",
+            opacity: "0"}
         ];
         let s2_animate = [
-            {transform: "translate(100vw, 0)"},
-            {transform: "translate(0, 0)"}
+            {transform: "translate(100px, 0)",
+                opacity: "0"},
+            {transform: "translate(0, 0)",
+                opacity: "1"}
         ];
         let timings = {
             duration: 300,
-            easing: "ease-out"
+            easing: "ease-out",
+            fill: "forwards"
         }
         let scene1 = document.getElementById("scene-1");
         let scene2 = document.getElementById("scene-2");
-
         scene2.style.display = "flex";
 
         scene1.animate(s1_animate, timings);
--- js/scene2.js
+++ js/scene2.js
@@ -2,29 +2,34 @@ const SCENE_AFFILIATION = "affiliation";
 const SCENE_SKILLS = "skills";
 const SCENE_AWARDS = "awards";
 let now_scene = SCENE_AFFILIATION;
-window.onload = function(){
+window.addEventListener('load', () => {
     bind_info_buttons();
-}
-function bind_info_buttons(){
+});
+
+function bind_info_buttons() {
     let btn_aff = [document.getElementById("btn_affiliation"), SCENE_AFFILIATION];
     let btn_skills = [document.getElementById("btn_skills"), SCENE_SKILLS];
     let btn_awards = [document.getElementById("btn_awards"), SCENE_AWARDS];
     let btns = [btn_aff, btn_skills, btn_awards];
-    for(let b in btns){
+    for (let b in btns) {
         let btn = btns[b];
         btn[0].onclick = function () {
             let target_scene = btn[1];
             let scenes = document.getElementsByClassName("transcene");
             for (let scene of scenes) {
-                if(scene.id !== target_scene){
+                if (scene.id !== target_scene) {
                     scene.style.display = "none";
                 } else {
                     scene.style.display = "flex";
                     let animation = [
-                        {transform: 'translate(-50px, 0px)',
-                        opacity: ".0"},
-                        {transform: 'translate(0px, 0px)',
-                        opacity: "1"}
+                        {
+                            transform: 'translate(-50px, 0px)',
+                            opacity: ".0"
+                        },
+                        {
+                            transform: 'translate(0px, 0px)',
+                            opacity: "1"
+                        }
                     ]
                     let timings = {
                         duration: 300,
